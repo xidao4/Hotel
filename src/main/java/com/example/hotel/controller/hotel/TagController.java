edcc252b495e755ea7e8e263a8c4f7f2752ca5cf
xidao4 <181250086@smail.nju.edu.cn>
2020-06-02 19:00:40 +0800

    feat: 解决bug ljy

--- src/main/java/com/example/hotel/controller/hotel/TagController.java
+++ src/main/java/com/example/hotel/controller/hotel/TagController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
+
 @RestController()
 @RequestMapping("/tags")
 public class TagController {
@@ -20,9 +22,9 @@ public class TagController {
     }
 
 
-    @PostMapping("/{tagName}/save")
-    public ResponseVO save(@PathVariable String tagName){
-        return tagService.save(tagName);
+    @PostMapping("/save")
+    public ResponseVO save(@PathParam("tagName") String tagName, @PathParam("hotelId") Integer hotelId){
+        return tagService.save(tagName,hotelId);
     }
 
     @PostMapping("/{relationId}/delete")
